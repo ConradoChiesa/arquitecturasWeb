e5b52ce42aeeda61997e7a1da8f1f860a59d46f5
joaquin.mancini <mancinijoaquinandres@gmail.com>
2021-11-23 23:40:56 -0300

    se aniade anotacion para permitir crossorigin

--- despensa/src/main/java/com/grupo10/despensa/controller/SaleController.java
+++ despensa/src/main/java/com/grupo10/despensa/controller/SaleController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path="/sales")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SaleController {
 
     @Autowired
